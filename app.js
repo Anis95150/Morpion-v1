e263eae87e1c8b79f4aa0ffbb8a727c87bf9243d
Anis95150 <32640183+Anis95150@users.noreply.github.com>
2018-03-09 15:49:47 +0100

    1st version morpion

---
+++ app.js
@@ -0,0 +1,63 @@
+let i = 0;
+
+const cases = document.querySelectorAll('.case');
+
+function tour(){
+    
+    i++;
+    return i % 2 === 0 ? 'O' : 'X';
+
+}
+
+function afficherGagnant(gagnant) {
+
+alert(`${gagnant} a gagner`);
+cases.forEach(el => el.innerHTML = '');
+
+
+}
+
+function isGagner(){
+
+    if(cases[0].innerHTML !=='' && cases[0].innerHTML === cases[1].innerHTML && cases[1].innerHTML === cases[2].innerHTML) {
+        afficherGagnant(cases[0].innerHTML);
+    }
+
+    else if(cases[3].innerHTML !=='' && cases[3].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[5].innerHTML) {
+        afficherGagnant(cases[3].innerHTML);
+}
+
+else if(cases[6].innerHTML !=='' && cases[6].innerHTML === cases[7].innerHTML && cases[7].innerHTML === cases[8].innerHTML) {
+    afficherGagnant(cases[6].innerHTML);
+}
+
+else if(cases[0].innerHTML !=='' && cases[0].innerHTML === cases[3].innerHTML && cases[3].innerHTML === cases[6].innerHTML) {
+    afficherGagnant(cases[0].innerHTML);
+}
+
+else if(cases[1].innerHTML !=='' && cases[1].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[7].innerHTML) {
+    afficherGagnant(cases[1].innerHTML);
+}
+
+else if(cases[2].innerHTML !=='' && cases[2].innerHTML === cases[5].innerHTML && cases[5].innerHTML === cases[8].innerHTML) {
+    afficherGagnant(cases[2].innerHTML);
+}
+
+
+else if(cases[0].innerHTML !=='' && cases[0].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[8].innerHTML) {
+    afficherGagnant(cases[0].innerHTML);
+}
+
+else if(cases[2].innerHTML !=='' && cases[2].innerHTML === cases[4].innerHTML && cases[4].innerHTML === cases[6].innerHTML) {
+    afficherGagnant(cases[2].innerHTML);
+}
+
+}
+
+function jouer(){
+
+    if(!this.innerHTML) this.innerHTML = tour();
+    isGagner();
+}
+
+cases.forEach( el => el.addEventListener('click', jouer));
